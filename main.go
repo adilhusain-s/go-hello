b4bc4096111bb1684b3519610548189fad054bd3
adilhusain-s <Adilhusain.Shaikh@ibm.com>
2022-06-23 14:07:12 +0530

    adding support for multi arch docker images

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
-
+import (
+        "fmt"
+        "runtime"
+)
 func main() {
-	fmt.Println("hello world!")
+        fmt.Println("Hello World")
+        fmt.Println(runtime.GOOS, runtime.GOARCH)
 }
